eec17476ddf222d49a87d1c1911e84f35cca7b86
Halid <denievhalid88@mail.ru>
2022-01-12 05:13:49 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,9 @@ import VSearchPanel from "./components/VSearchPanel";
 
 export default {
   components: { VHeader, VSearchPanel },
+  mounted() {
+    document.title =
+      "Работа в Москве, поиск персонала и публикация вакансий - hh.ru";
+  },
 };
 </script>
