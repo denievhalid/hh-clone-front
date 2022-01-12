a4f07099c1cbd49f259e47c61c3d598d3b773a39
Halid <denievhalid88@mail.ru>
2022-01-12 05:23:53 +0300

    commit

--- src/components/VFormGroup/index.vue
+++ src/components/VFormGroup/index.vue
@@ -15,5 +15,15 @@ export default {};
   & > * {
     border-radius: 0;
   }
+
+  & > *:first-child {
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+
+  & > *:last-child {
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
 }
 </style>
