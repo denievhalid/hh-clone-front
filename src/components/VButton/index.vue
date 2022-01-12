d4dee1637855061b7c6588702592d5ad643531af
Halid <denievhalid88@mail.ru>
2022-01-12 05:27:25 +0300

    commit

--- src/components/VButton/index.vue
+++ src/components/VButton/index.vue
@@ -25,16 +25,25 @@ export default {
 
 <style lang="scss">
 .btn {
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
   font-size: 14px;
   user-select: none;
   white-space: nowrap;
   background: transparent;
-  border: 0;
+  border: 1px solid transparent;
+  cursor: pointer;
 
   &--primary {
     color: #fff;
     border-color: #1785e5;
     background-color: #1785e5;
+  }
+
+  &:hover {
+    border-color: #167ed8;
+    background-color: #167ed8;
   }
 }
 </style>
